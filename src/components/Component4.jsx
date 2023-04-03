11551be1f6bca31a36138ffb6acff80469f477b5
davy <davylebris@gmail.com>
2023-04-03 12:06:17 +0200

    ajout exo props

---
+++ src/components/Component4.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component5 from './Component5';
+
+const Component4 = () => {
+  return (
+    <>
+      <div>Component4</div>
+      <Component5 />
+    </>
+  );
+};
+
+export default Component4;
---
+++ src/components/Component5.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Component5 = () => {
+
+  console.log('test')
+
+  return (
+    <>
+    <div>Component5</div>
+    </>
+  )
+}
+
+export default Component5
