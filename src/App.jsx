21448e38efa06bce3ed42d0382ece4b505d3556c
davy <davylebris@gmail.com>
2023-04-04 15:17:37 +0200

    correction exercice mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-import './App.css'
+import './App.css';
+import Destination from './components/Destination';
 
-const fruits = ["pomme", "banane", "kiwi", "ananas", "orange", "pêche"]
+const fruits = ['pomme', 'banane', 'kiwi', 'ananas', 'orange', 'pêche'];
 
 const address = [
   {
-    city: "Paris",
-    country: "France",
-    street: "place de la bourse"
+    city: 'Paris',
+    country: 'France',
+    street: 'place de la bourse',
   },
   {
-    city: "Londre",
-    country: "Angleterre",
-    street: "Rue des anglais"
+    city: 'Londre',
+    country: 'Angleterre',
+    street: 'Rue des anglais',
   },
   {
-    city: "Madrid",
-    country: "Espagne",
-    street: "rue des espagnol"
+    city: 'Madrid',
+    country: 'Espagne',
+    street: 'rue des espagnol',
   },
-]
+];
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {fruits.map((fruit) => (
+//         <h5>{fruit}</h5>
+//       ))}
+//     </div>
+//   );
+// }
 
 function App() {
-
   return (
     <div className="App">
-
+      {address.map((response) => (
+        <Destination detailsAddress={response} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Destination = () => {
-
+const Destination = ({ detailsAddress }) => {
+  console.log(detailsAddress);
   return (
     <div>
-      <h3>le pays est ??? </h3>
-      <h3>la ville est ??? </h3>
-      <h3>la rue est ??? </h3>
+      <h3>le pays est {detailsAddress.country}</h3>
+      <h3>la ville est {detailsAddress.city} </h3>
+      <h3>la rue est {detailsAddress.street} </h3>
     </div>
   );
-}
+};
 
 export default Destination
